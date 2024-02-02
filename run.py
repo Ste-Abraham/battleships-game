d8a6f69f0bfa9d8e728347c77c160ac6133d20c4
Ste-Abraham <>
2024-02-02 11:35:16 +0000

    Add the size of the 5 different vessels that will be in play

--- run.py
+++ run.py
@@ -6,3 +6,6 @@ PLAYER1_BOARD = [[" "] * 7 for i in range(7)]
 COMPUTER_BOARD = [[" "] * 7 for i in range(7)]
 PLAYER1_CHOICE = [[" "] * 7 for i in range(7)]
 COMPUTER_CHOICE = [[" "] * 7 for i in range(7)]
+
+# Size of each vessel in gameboard area
+VESSEL_SIZE = [1, 1, 2, 2, 3,]
