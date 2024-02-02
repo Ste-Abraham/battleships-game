cc7eb8cad1a86e5cea4dc574f2c2f34bfa43298c
Ste-Abraham <>
2024-02-02 11:33:49 +0000

    Add code for gameboards

--- run.py
+++ run.py
@@ -1,2 +1,8 @@
 # Import libraries used for 'randint' method
 import random
+
+# Gameboards - 4 needed for the game to run
+PLAYER1_BOARD = [[" "] * 7 for i in range(7)]
+COMPUTER_BOARD = [[" "] * 7 for i in range(7)]
+PLAYER1_CHOICE = [[" "] * 7 for i in range(7)]
+COMPUTER_CHOICE = [[" "] * 7 for i in range(7)]
